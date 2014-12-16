71be88c5872c3d3b66c214bb4ef4bcae0190f6ea
alex.kooper@gmail.com <alex.kooper@gmail.com>
2014-12-16 17:54:25 -0500

    Add Polyomino class

--- scala/polyomino/src/main/scala/kooper/polyomino/Point.scala
+++ scala/polyomino/src/main/scala/kooper/polyomino/Point.scala
@@ -1,6 +1,6 @@
 package kooper.polyomino
 
-case class Point(val x: Int, val y: Int) extends Equals {
+case class Point(val x: Int, val y: Int) extends Ordered[Point] {
   
   def move(dx: Int, dy: Int) = new Point(x + dx, y + dy)
   
@@ -14,6 +14,15 @@ case class Point(val x: Int, val y: Int) extends Equals {
      val newX = (this.y - p.y) + p.x
      val newY = -(this.x - p.x) + p.y
      new Point(newX, newY);   
+  }
+  
+  override def compare(that: Point) = {
+    val compareX = this.x compare that.x
+    
+    if(compareX == 0) 
+      this.y compare that.y 
+    else 
+      compareX
   }
 }
 
