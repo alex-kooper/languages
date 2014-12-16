9fb95cc7c223409556f48db5abc3a949a346da3a
alex.kooper@gmail.com <alex.kooper@gmail.com>
2014-12-16 14:27:25 -0500

    Finished Point class

--- scala/polyomino/src/main/scala/kooper/polyomino/Point.scala
+++ scala/polyomino/src/main/scala/kooper/polyomino/Point.scala
@@ -1,5 +1,20 @@
 package kooper.polyomino
 
-class Point(val x: Int, y: Int) {
+class Point(val x: Int, val y: Int) {
+  
   def move(dx: Int, dy: Int) = new Point(x + dx, y + dy)
+  
+  def rotateRight(p: Point) = {
+    val newX = -(this.y - p.y) + p.x
+    val newY = (this.x - p.x) + p.y;
+    new Point(newX, newY);   
+  } 
+    
+  def rotateLeft(p: Point) = {
+     val newX = (this.y - p.y) + p.x
+     val newY = -(this.x - p.x) + p.y
+     new Point(newX, newY);   
+  }
+  
+  override def toString = "Point(" + this.x + ", " + this.y + ")"
 }
