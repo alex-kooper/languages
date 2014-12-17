d9bb91dc26ea286057c79a6f43e5f36011a912db
alex.kooper@gmail.com <alex.kooper@gmail.com>
2014-12-17 00:00:31 -0500

    implemented toString

--- scala/polyomino/src/main/scala/kooper/polyomino/Polyomino.scala
+++ scala/polyomino/src/main/scala/kooper/polyomino/Polyomino.scala
@@ -14,12 +14,32 @@ case class Polyomino(val points: Set[Point]) {
     new Point(x, y)
   }
   
-  def move(dx: Int, dy: Int) = points.map(_.move(dx, dy))
-  def rotateRight(p: Point) = points.map(_.rotateRight(p))
-  def rotateLeft(p: Point) = points.map(_.rotateLeft(p))
+  def width = lowerRightCorner.x - upperLeftCorner.x + 1
+  def height = lowerRightCorner.y - upperLeftCorner.y + 1
+  
+  def move(dx: Int, dy: Int) = new Polyomino(points.map(_.move(dx, dy)))
+  def rotateRight(p: Point) = new Polyomino(points.map(_.rotateRight(p)))
+  def rotateLeft(p: Point) = new Polyomino(points.map(_.rotateLeft(p)))
   
   def normalize = {
     val Point(x, y) = upperLeftCorner
     this.move(-x, -y)
-  }  
+  }
+  
+  override def toString = {
+    val n = this.normalize
+    
+    var a = Array.tabulate(height, width)((y, x) => 
+      if(n.points.contains(Point(x, y))) "[]" else "  "
+    )
+    
+    a.map(_.mkString).mkString("\n", "\n", "\n")
+  }
+}
+
+object Polyomino {
+  def apply(xs: (Int, Int)*) = {
+    val points = for((x,y) <-xs) yield Point(x, y)
+    new Polyomino(points.toSet)  
+  }
 }
