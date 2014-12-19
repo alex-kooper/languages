73dab782f0c0ccf4a6736ce3be0cb9d677ab0426
alex.kooper@gmail.com <alex.kooper@gmail.com>
2014-12-18 19:42:21 -0500

    Simplified the generator

--- scala/polyomino/src/main/scala/kooper/polyomino/Generator.scala
+++ scala/polyomino/src/main/scala/kooper/polyomino/Generator.scala
@@ -4,29 +4,24 @@ import scala.io.StdIn
 
 object Generator {
   
-  def generate(nBlocks: Int): Set[Polyomino] = {
-    if(nBlocks == 1) 
+  def generate(nPoints: Int): Set[Polyomino] = {
+    if(nPoints == 1) 
       Set(Polyomino((0, 0)))
     else    
-      generate(nBlocks - 1).map(generateAllOfPlusOneBlock(_)).flatten
+      generate(nPoints - 1).flatMap(generateByAddingOnePoint(_))
   }
   
-  private def generateByAddingBlocksAround(polyomino: Polyomino, point: Point) = {
+  private def generateByAddingOnePoint(polyomino: Polyomino) = {
     val adjacentPointDeltas = Set((-1, 0), (0, -1), (1, 0), (0, 1))
     
     for {
+      p <- polyomino.points
       (dx, dy) <- adjacentPointDeltas
-      newPoint = point.move(dx, dy)
+      newPoint = p.move(dx, dy)
       if(!polyomino.contains(newPoint))
     } yield (polyomino + newPoint).normalize
   }
   
-  private def generateAllOfPlusOneBlock(polyomino: Polyomino) = {
-    polyomino.points.toSet
-      .map((p: Point) => generateByAddingBlocksAround(polyomino, p))
-      .flatten
-  }
-
   def main(args: Array[String]) {
     print("Enter number of blocks: ")
     val n = StdIn.readInt
