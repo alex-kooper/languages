63267473133f7f8281234183f62c36200ff633b5
alex.kooper@gmail.com <alex.kooper@gmail.com>
2014-12-27 12:31:57 -0500

    Simplified generator

--- haskell/Polyomino/Generator.hs
+++ haskell/Polyomino/Generator.hs
@@ -4,22 +4,19 @@ import Data.List
 import Polyomino
 import qualified Point
 
-adjacentPointDeltas = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-
-generateBasedOnPoint :: Polyomino -> Point.Point -> [Polyomino]
-generateBasedOnPoint polyomino point = [normalize $ add newPoint polyomino | 
-                                        (dx, dy) <- adjacentPointDeltas, 
-                                        let newPoint = Point.move dx dy point,
-                                        not (newPoint `belongsTo` polyomino)]
-
-generateBasedOn :: Polyomino -> [Polyomino] 
-generateBasedOn polyomino = concat [generateBasedOnPoint polyomino p |
-                                    p <- Set.toList $ points polyomino]
+generateByAddingOnePoint :: Polyomino -> [Polyomino]
+generateByAddingOnePoint polyomino = [normalize $ add newPoint polyomino |
+                                      p <- Set.toList $ points polyomino,
+                                      (dx, dy) <- adjacentPointDeltas, 
+                                      let newPoint = Point.move dx dy p,
+                                      not (newPoint `belongsTo` polyomino)]
+    where
+        adjacentPointDeltas = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
 generatePolyominos :: Int -> [Polyomino]
 
 generatePolyominos 1 = [fromList [(0, 0)]]
 
 generatePolyominos n = 
-    nub $ concat [generateBasedOn p | p <- generatePolyominos (n - 1)]
+    nub $ concat [generateByAddingOnePoint p | p <- generatePolyominos (n - 1)]
 
