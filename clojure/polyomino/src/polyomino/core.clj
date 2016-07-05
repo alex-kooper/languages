74ab3b3522ad4e6ad1b1452b9d2376c789f629e3
alex.kooper@gmail.com <alex.kooper@gmail.com>
2016-07-05 00:50:05 -0400

    Added execution time measurement

--- clojure/polyomino/src/polyomino/core.clj
+++ clojure/polyomino/src/polyomino/core.clj
@@ -20,16 +20,30 @@
       (comp set (partial mapcat generate-by-adding-one-point))
       [(polyomino/from-pairs [0 0])])
     (dec n)))
-   
+
+(defn format-duration [d]
+  (let [duration-in-seconds (/ (double d) 1000000000)
+        hours               (int (quot duration-in-seconds 3600))
+        hours-r             (rem duration-in-seconds 3600)
+        minutes             (int (quot hours-r 60))
+        seconds             (rem hours-r 60)]  
+    (format "%d:%02d:%f" hours minutes seconds)))
+
 (defn -main [& args]
   (print "Enter number of cells: ")(flush)
 
   (let [n (read-string (read-line))
-        polyominos (generate-polyominos n)]
+        start-time (System/nanoTime)
+        polyominos (generate-polyominos n)
+        end-time (System/nanoTime)
+        duration (- end-time start-time)]
     (do
-      (println (format 
-        "There are %d polyominos with %d cells." 
-        (count polyominos) n))          
+      (println (format "There are %d polyominos with %d cells." 
+                       (count polyominos)
+                       n))          
+
+      (println (format "It took %s seconds to generate them." 
+                       (format-duration duration)))
 
       (print "Would you like to see all of them [y/n]?")(flush)
 
