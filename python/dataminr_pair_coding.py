f5300f0c7db4c779e105a3fabe8f388dda15434a
alex.kooper@gmail.com <alex.kooper@gmail.com>
2018-11-16 23:16:18 -0500

    Shortened the solution by using regular expressions.

--- python/dataminr_pair_coding.py
+++ python/dataminr_pair_coding.py
@@ -4,30 +4,18 @@ IDs can be either for Android or IPhone.
 Android format is A + 4 characters.
 IPhone format is I + 3 characters. 
 """
+import re
 from itertools import takewhile, dropwhile, izip
 
 EXAMPLE = "Aa1133Iab2Aa112Iac3"
 
-def tokens(input_string):
-    rest = input_string
-
-    while rest:
-        if rest.startswith('A'):
-            yield rest[0:4]
-            rest = rest[4:]
-        elif rest.startswith('I'):
-            yield rest[0:3]
-            rest = rest[3:]
-        elif rest[0].isdigit:
-            i = takewhile(lambda c: c.isdigit(), rest)
-            yield int(''.join(i))
-            rest = ''.join(dropwhile(lambda c: c.isdigit(), rest))
-        else:
-            raise Exception("Invalid String!")
+def tokens(s):
+    pattern = r'A\w{3}|I\w{2}|\d+' 
+    return (g.group() for g in re.finditer(pattern, s))
 
 def tokens_to_pairs(tokens):
     i = iter(tokens)
-    return izip(i, i)
+    return ((ident, int(quant)) for (ident, quant) in izip(i, i))
 
 def parse_into_map(s):
     d = {}
