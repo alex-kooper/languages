fab6821e5975c11c3e6bf0ecc4630b0d94be4b89
alex.kooper@gmail.com <alex.kooper@gmail.com>
2020-02-29 14:44:02 -0500

    Added task explanation and a better example.

--- scala/src/main/scala/VestwellCoding.scala
+++ scala/src/main/scala/VestwellCoding.scala
@@ -1,12 +1,19 @@
+/*
+ Given a list of n string containing letters. Find the size of largest subset of string which are
+ anagram of each others.
+ An anagram of a string is another string that contains same characters, only the order of characters
+ can be different. For example, “abcd” and “dabc” are anagram of each other.
+*/
 
 object VestwellCoding extends App {
-  val words = Seq(
+  val words = Stream(
+    "Ant",
+    "Magenta",
+    "Magnate",
+    "Tan",
+    "Gnamate",
     "Cat",
-    "Tac",
-    "Poos",
-    "Oops",
-    "Soop",
-    "Test"
+    "Tac"
   )
 
   def makeKey(word: String) = word.toLowerCase.sorted
@@ -16,5 +23,5 @@ object VestwellCoding extends App {
     .values
     .maxBy(_.length)
 
-  println(s"Largest Anagram Group ${largestAnagramGroup(words.toStream)}")
+  println(s"Largest Anagram Group ${largestAnagramGroup(words)}")
 }
