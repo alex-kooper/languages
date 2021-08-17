a2c07b9d4899f3bef7ebec05d05810cbd698d7f5
Alex Kuprienko <alex.kuprienko@tqtezos.com>
2021-08-17 17:27:07 -0400

    Configure ocamlformat and apply to the source.

--- ocaml/sudoku/lib/data.ml
+++ ocaml/sudoku/lib/data.ml
@@ -1,31 +1,34 @@
-
 module Cell = struct
   type t = int * int
+
   let compare = compare
 end
 
 module Grid = struct
-  module Map = Map.Make(Cell)
+  module Map = Map.Make (Cell)
 
   type t = int Map.t
 
   let empty = Map.empty
+
   let set ~row ~col ~digit = Map.add (row, col) digit
+
   let get ~row ~col = Map.find_opt (row, col)
 end
 
 module GridConstraints = struct
-  module Digits = Set.Make(Int)
-  module Map = Map.Make(Cell)
+  module Digits = Set.Make (Int)
+  module Map = Map.Make (Cell)
 
   type t = Digits.t Map.t
 
   let empty = Map.empty
+
   let set ~row ~col ~digits = Map.add (row, col) digits
+
   let remove ~row ~col = Map.remove (row, col)
 
-  let remove_digit ~row ~col ~digit = 
-    Map.update 
-      (row, col) 
-      (fun digits -> Option.map (Digits.remove digit) digits)
+  let remove_digit ~row ~col ~digit =
+    Map.update (row, col) (fun digits ->
+        Option.map (Digits.remove digit) digits)
 end
--- ocaml/sudoku/lib/data.mli
+++ ocaml/sudoku/lib/data.mli
@@ -2,17 +2,22 @@ module Grid : sig
   type t
 
   val empty : t
+
   val set : row:int -> col:int -> digit:int -> t -> t
+
   val get : row:int -> col:int -> t -> int option
 end
 
-module GridConstraints : sig 
+module GridConstraints : sig
   type t
 
   module Digits : Set.S with type elt = int
 
   val empty : t
+
   val set : row:int -> col:int -> digits:Digits.t -> t -> t
+
   val remove : row:int -> col:int -> t -> t
+
   val remove_digit : row:int -> col:int -> digit:int -> t -> t
 end
