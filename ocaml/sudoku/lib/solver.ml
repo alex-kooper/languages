3deceec215ae3f34cfef00db1b6e6911c94a7557
Alex Kuprienko <alex.kuprienko@tqtezos.com>
2021-08-17 19:24:23 -0400

    Partiallly implement solver.

---
+++ ocaml/sudoku/lib/solver.ml
@@ -0,0 +1,44 @@
+open Data
+
+let ( let* ) x f = Base.Sequence.bind x ~f
+
+let guard x = Base.Sequence.(if x then singleton () else empty)
+
+module Cell = struct
+  type t = int * int
+
+  let compare = compare
+end
+
+module Cell_set = Set.Make (Cell)
+
+let unknown_cells grid =
+  let open Base.Sequence in
+  let* row = range 1 10 in
+  let* col = range 1 10 in
+  let* () = guard (grid |> Grid.get ~row ~col |> Option.is_none) in
+
+  return (row, col)
+
+let related_cells ~row ~col =
+  let open Base.Sequence in
+  let row_cells =
+    range 1 10 |> map ~f:(fun col -> (row, col)) |> to_seq |> Cell_set.of_seq
+  in
+
+  let col_cells =
+    range 1 10 |> map ~f:(fun row -> (row, col)) |> to_seq |> Cell_set.of_seq
+  in
+
+  let subgrid_first x = ((x - 1) / 3 * 3) + 1 in
+  let subgrid_last x = subgrid_first x + 3 in
+
+  let subgrid_cells =
+    (let* row = range (subgrid_first row) (subgrid_last row) in
+     let* col = range (subgrid_first col) (subgrid_last col) in
+
+     return (row, col))
+    |> to_seq |> Cell_set.of_seq
+  in
+
+  row_cells |> Cell_set.union col_cells |> Cell_set.union subgrid_cells
