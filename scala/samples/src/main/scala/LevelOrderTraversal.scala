9b0b9168bcbb051cab2f60d3caa352f8091ffcfb
Alex Kuprienko <alex.kuprienko@tqtezos.com>
2021-11-15 20:12:25 -0500

    Add function mkNode to be able to take default parameter values.

--- scala/samples/src/main/scala/LevelOrderTraversal.scala
+++ scala/samples/src/main/scala/LevelOrderTraversal.scala
@@ -1,19 +1,24 @@
 // Trees: Level order traversal of binary tree
 
-// Given the root of a binary tree, display the node values at each level. 
-// Node values for all levels should be displayed on separate lines. 
+// Given the root of a binary tree, display the node values at each level.
+// Node values for all levels should be displayed on separate lines.
 
-enum BinaryTree[+T]:
-  case Node(value: T, left: BinaryTree[T] = Leaf, right: BinaryTree[T] = Leaf)
+enum BTree[+T]:
+  case Node(value: T, left: BTree[T], right: BTree[T])
   case Leaf
 
   def isLeaf: Boolean = this match
     case Leaf => true
     case _    => false
 
-import BinaryTree.*
+import BTree.{Node, Leaf}
 
-def nextLevel[T](level: Seq[BinaryTree[T]]) =
+// mkNode only necessary because Node constructor does not work with
+// default parameter values. I suspect it is a bug in Scala3.
+def mkNode[T](value: T, left: BTree[T] = Leaf, right: BTree[T] = Leaf) =
+  BTree.Node(value, left, right)
+
+def nextLevel[T](level: Seq[BTree[T]]) =
   level
     .flatMap { t =>
       t match
@@ -22,14 +27,14 @@ def nextLevel[T](level: Seq[BinaryTree[T]]) =
     }
     .filter(!_.isLeaf)
 
-def levelOrderTraversal[T](root: BinaryTree[T]) =
+def levelOrderTraversal[T](root: BTree[T]) =
   LazyList.iterate(Seq(root))(nextLevel).takeWhile(_.nonEmpty)
 
-def nodeToString[T](n: BinaryTree[T]) = n match
+def nodeToString[T](n: BTree[T]) = n match
   case Leaf              => ""
   case Node(value, _, _) => value.toString
 
-def levelOrderTraversalToString[T](root: BinaryTree[T]) =
+def levelOrderTraversalToString[T](root: BTree[T]) =
   levelOrderTraversal(root)
     .map { level =>
       level.map(nodeToString).mkString(", ")
@@ -37,17 +42,18 @@ def levelOrderTraversalToString[T](root: BinaryTree[T]) =
     .mkString("\n")
 
 @main def printLevelOrderTraversal =
-  val tree = Node(
+  val tree = mkNode(
     100,
-    Node(
+    mkNode(
       50,
-      new Node(25, Leaf, Leaf),
-      Node(75, Leaf, Leaf)
+      mkNode(25),
+      mkNode(75)
     ),
-    Node(
+    mkNode(
       200,
       Leaf,
-      Node(350, Leaf, Leaf)
+      mkNode(350)
     )
   )
+
   println(levelOrderTraversalToString(tree))
