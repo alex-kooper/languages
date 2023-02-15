a6fd499f8fd55c74da0464f5365c8ec0f3dbbc50
Alex Kuprienko <akuprienko@williamhill.us>
2023-02-15 13:07:27 -0500

    Implement polyomino generator and its test

--- rust/polyomino/src/generator.rs
+++ rust/polyomino/src/generator.rs
@@ -1,6 +1,31 @@
 #![allow(dead_code)]
 
+use im::OrdSet;
+use std::iter::successors;
+
 use crate::{point::Coordinate, polyomino::*};
+
+pub fn generate(n: usize) -> OrdSet<Polyomino> {
+    let monomino: Polyomino = Polyomino::from([(0, 0)]);
+    let domino: Polyomino = Polyomino::from([(0, 0), (1, 0)]);
+
+    match n {
+        0 => OrdSet::new(),
+        1 => OrdSet::unit(monomino),
+        2 => OrdSet::unit(domino),
+        n => successors(Some(OrdSet::unit(domino)), |polyominos| {
+            Some(
+                polyominos
+                    .iter()
+                    .flat_map(|p| generate_by_adding_one_point_to(p))
+                    .collect(),
+            )
+        })
+        .take(n - 1)
+        .last()
+        .unwrap(),
+    }
+}
 
 fn generate_by_adding_one_point_to(polyomino: &Polyomino) -> impl Iterator<Item = Polyomino> + '_ {
     const ADJACENT_POINT_DELTAS: [(Coordinate, Coordinate); 4] = [(-1, 0), (0, -1), (1, 0), (0, 1)];
@@ -23,15 +48,19 @@ fn generate_by_adding_one_point_to(polyomino: &Polyomino) -> impl Iterator<Item 
 
 #[cfg(test)]
 mod tests {
-    use im::OrdSet;
     use super::*;
+    use im::OrdSet;
 
     #[test]
     pub fn test_generated_by_adding_one_point() {
         let polyomino = Polyomino::from([(0, 0), (1, 0)]).to_canonical_form();
         let polyominos: OrdSet<Polyomino> = generate_by_adding_one_point_to(&polyomino).collect();
-        
+
         assert_eq!(polyominos.len(), 2)
     }
 
+    #[test]
+    pub fn test_generate_tetramino() {
+        assert_eq!(generate(4).len(), 5)
+    }
 }
--- rust/polyomino/src/main.rs
+++ rust/polyomino/src/main.rs
@@ -1,22 +1,9 @@
+mod generator;
 mod point;
 mod polyomino;
-mod generator;
-
-// use point::Point;
-// use polyomino::Polyomino;
 
 fn main() {
-    // let mut p = Point::new(1, 2);
-
-    // p.shift(1, 1);
-    // p.rotate_left(&Point::ORIGIN);
-    // p.rotate_right(&Point::ORIGIN);
-    // p.rotate_left(&Point::ORIGIN);
-    // p.rotate_left(&Point::ORIGIN);
-    // p.rotate_left(&Point::ORIGIN);
-    // p.rotate_left(&Point::ORIGIN);
-
-    // let _polyomino = Polyomino::from([p.clone(), p.clone()]);
-    
-    // println!("Point: {:?}", p);
+    for p in generator::generate(10) {
+        print!("{p}")
+    }
 }
