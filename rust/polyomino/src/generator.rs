b44ecab81e26041ba24f01be91f26464992b7ca5
Alex Kuprienko <akuprienko@williamhill.us>
2023-02-15 10:44:38 -0500

    Add a unit test

--- rust/polyomino/src/generator.rs
+++ rust/polyomino/src/generator.rs
@@ -2,7 +2,7 @@
 
 use crate::{point::Coordinate, polyomino::*};
 
-fn generate_by_adding_one_point(polyomino: &Polyomino) -> impl Iterator<Item = Polyomino> + '_ {
+fn generate_by_adding_one_point_to(polyomino: &Polyomino) -> impl Iterator<Item = Polyomino> + '_ {
     const ADJACENT_POINT_DELTAS: [(Coordinate, Coordinate); 4] = [(-1, 0), (0, -1), (1, 0), (0, 1)];
 
     polyomino
@@ -14,9 +14,24 @@ fn generate_by_adding_one_point(polyomino: &Polyomino) -> impl Iterator<Item = P
                 if polyomino.contains(new_point) {
                     None
                 } else {
-                    Some(polyomino.add_point(*point))
+                    Some(polyomino.add_point(new_point).to_canonical_form())
                 }
             })
         })
         .flatten()
+}
+
+#[cfg(test)]
+mod tests {
+    use im::OrdSet;
+    use super::*;
+
+    #[test]
+    pub fn test_generated_by_adding_one_point() {
+        let polyomino = Polyomino::from([(0, 0), (1, 0)]).to_canonical_form();
+        let polyominos: OrdSet<Polyomino> = generate_by_adding_one_point_to(&polyomino).collect();
+        
+        assert_eq!(polyominos.len(), 2)
+    }
+
 }
